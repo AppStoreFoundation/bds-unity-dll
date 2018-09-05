c21984b0366f3f0364e7376fac2ff7b1be430d69
Fábio Vital <fabio.vital@aptoide.com>
2018-09-05 10:01:08 +0100

    Fix (Bash Scripts): new out paths

--- Scripts/Bash/Unity2018.sh
+++ Scripts/Bash/Unity2018.sh
@@ -2,6 +2,6 @@ mkdir -p ../bds-unity-plugin/AppcoinsUnityPlugin/Assets/AppcoinsUnity/Scripts
 mkdir -p ../bds-unity-plugin/AppcoinsUnityPlugin/Assets/AppcoinsUnity/Scripts/Editor
 cp ./unity-2018/bin/Debug/bds-unity-2018.dll ../bds-unity-plugin/AppcoinsUnityPlugin/Assets/AppcoinsUnity/Scripts
 cp ./unity-editor-2018/bin/Debug/bds-unity-editor-2018.dll ../bds-unity-plugin/AppcoinsUnityPlugin/Assets/AppcoinsUnity/Scripts/Editor
-cp ./unity-editor-2018/bin/Debug/appcoins-unity-support-2018.dll ../bds-unity-plugin/AppcoinsUnityPlugin/Assets/AppcoinsUnity/Scripts/Editor
+cp ./unity-editor-2018/bin/Debug/AppCoinsUnitySupport2018.dll ../bds-unity-plugin/AppcoinsUnityPlugin/Assets/AppcoinsUnity/Scripts/Editor
 cp ./Scripts/ToCopy/* ../bds-unity-plugin/AppcoinsUnityPlugin/Assets/AppcoinsUnity/Scripts
 cp ./Scripts/ToCopy/Editor/* ../bds-unity-plugin/AppcoinsUnityPlugin/Assets/AppcoinsUnity/Scripts/Editor
--- Scripts/Bash/Unity5_6.sh
+++ Scripts/Bash/Unity5_6.sh
@@ -2,6 +2,6 @@ mkdir -p ../bds-unity-plugin-5_6/AppcoinsUnityPlugin/Assets/AppcoinsUnity/Script
 mkdir -p ../bds-unity-plugin-5_6/AppcoinsUnityPlugin/Assets/AppcoinsUnity/Scripts/Editor
 cp ./unity-5.6/bin/Debug/bds-unity-5.6.dll ../bds-unity-plugin-5_6/AppcoinsUnityPlugin/Assets/AppcoinsUnity/Scripts
 cp ./unity-editor-5.6/bin/Debug/bds-unity-editor-5.6.dll ../bds-unity-plugin-5_6/AppcoinsUnityPlugin/Assets/AppcoinsUnity/Scripts/Editor
-cp ./unity-editor-5.6/bin/Debug/appcoins-unity-support-5_6.dll ../bds-unity-plugin-5_6/AppcoinsUnityPlugin/Assets/AppcoinsUnity/Scripts/Editor
+cp ./unity-editor-5.6/bin/Debug/AppCoinsUnitySupport5_6.dll ../bds-unity-plugin-5_6/AppcoinsUnityPlugin/Assets/AppcoinsUnity/Scripts/Editor
 cp ./Scripts/ToCopy/* ../bds-unity-plugin-5_6/AppcoinsUnityPlugin/Assets/AppcoinsUnity/Scripts
 cp ./Scripts/ToCopy/Editor/* ../bds-unity-plugin-5_6/AppcoinsUnityPlugin/Assets/AppcoinsUnity/Scripts/Editor
