978ae16a42ed1fe5035f57872eec7b524628a8ff
Mikie Ribeiro <mikie.ribeiro@aptoide.com>
2018-11-29 09:42:14 +0000

    Automatic background setup
    APPC-738 2018 Build Process
    
    Setup is now being deployed immediately after the plugin is imported and every time the prefab name is changed by the developer. Changes only made for unity 2018 so far

---
+++ unity-2018/Store/PrefapUpdater.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+using Appcoins.Purchasing;
+
+
+[ExecuteInEditMode]
+public class PrefapUpdater : MonoBehaviour
+{
+    private AppcoinsPurchasing appcoinsPurchasing;
+    private string newPrefabName;
+    private bool newUseAdsSDK;
+    private bool newUseMainNet;
+
+
+    void Awake()
+    {
+        appcoinsPurchasing = GetComponent<AppcoinsPurchasing>();
+        newPrefabName = gameObject.name;
+        Debug.Log("awakee");
+        newUseAdsSDK = appcoinsPurchasing.UsesAdsSDK();
+        newUseMainNet = appcoinsPurchasing.UsesMainNet();
+    }
+
+    void Update()
+    {
+        bool setupValue = false;
+        if (!newPrefabName.Equals(gameObject.name))
+        {
+            Debug.Log("Name has been changed");
+            newPrefabName = gameObject.name;
+            setupValue = true;
+        }
+        else
+        {
+            Debug.Log("Name still the same");
+        }
+
+        if(!appcoinsPurchasing.UsesMainNet() || !appcoinsPurchasing.UsesAdsSDK())
+        {
+            setupValue = true;
+        }
+        Debug.Log("updateee");
+        if(setupValue.Equals(true))
+        {
+            Debug.Log("Setup function deployed");
+        }
+    }
+}
+
+
